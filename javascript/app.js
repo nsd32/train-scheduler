8edf0c16babff853a747ea8d7e541a73766550c2
Lea Abetya <leaabetya@Leas-MacBook-Air-2.local>
2017-08-25 16:08:09 -0600

    military converter working

--- javascript/app.js
+++ javascript/app.js
@@ -26,12 +26,20 @@ $(document).ready(function() {
   	var array = Object.keys(trains);
   	console.log(trains[array[0]].name);
 
+  	// var time = trains[array[0]].firstTrain;
+  	// console.log(time);
+  	// time = time.split(':');
+  	// console.log(time);
+  	// console.log(militaryTimeConverter(time));
+
   	for (var i = 0; i < array.length; i++) {
   		var key = array[i];
   		var name = $('<td>' + trains[key].name + '</td>');
   		var destination = $('<td>' + trains[key].destination + '</td>');
   		var frequency = $('<td>' + trains[key].frequency + '</td>');
-  		var nextArrival = $('<td>' + trains[key].firstTrain + '</td>');
+  		var time = trains[key].firstTrain;
+  		var nextArrival = $('<td>' + militaryTimeConverter(time) + '</td>');
+  		console.log(nextArrival)
   		var tableRow = $('<tr>');
   		
   		$('#tbody').prepend(tableRow);
@@ -52,7 +60,7 @@ $(document).ready(function() {
   $('#submit-button').on('click', function(event) {
  
     event.preventDefault();
-
+    // TODO make user type in valid military time
   	var train = {
   	  name: $('#train-name').val().trim(),
   	  destination: $('#destination').val().trim(),
@@ -69,5 +77,43 @@ $(document).ready(function() {
     console.log(train);
 
   });
+
+  function militaryTimeConverter(time) {
+  	time = time.split(':');
+  	var hours = Number(time[0]);
+  	var minutes = Number(time[1])
+
+  	var timeValue;
+
+  	if (hours > 0 && hours <= 12) {
+  	  timeValue = hours;
+  	} else if (hours > 12) {
+      timeValue = hours - 12;
+  	} else if (hours === 0) {
+      timeValue = 12;
+  	}
+
+  	timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;
+  	timeValue += (hours >= 12) ? " P.M." : " A.M.";
+
+  	return timeValue;
+  }
+
+  function minutesAway(time) {
+  	var currentTime = new Date();
+  	console.log(currentTime.getHours());
+
+  	currentTime = currentTime.getHours() + ':' + currentTime.getMinutes();
+  	console.log(currentTime)
+
+  }
+
+  minutesAway();
+
+  // $('#submit-button').keyup(function (event) {
+  // 	if (event.keyCode === 13) {
+  // 		$('#submit-button').click();
+  // 	}
+  // });
 
 });
